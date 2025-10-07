31a4520c31b53174792bb08b301d7d947719cdc5
Weiberlee <weiberlee2007918@gmail.com>
2025-10-07 20:15:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__30905__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return Flask('index.html')
 
 @app.route('/competition')
 def competition():
